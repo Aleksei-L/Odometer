e3b6b53e8c8d75d6135f2ded1407e7ecdcd37ffe
Aleksei Lobanov <lobanov782@gmail.com>
2023-08-27 21:09:20 +0300

    Updated OdometerService - use Android geolocation

--- app/src/main/java/com/example/odometer/OdometerService.kt
+++ app/src/main/java/com/example/odometer/OdometerService.kt
@@ -1,20 +1,73 @@
 package com.example.odometer
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Binder
 import android.os.IBinder
-import java.util.Random
+import androidx.core.app.ActivityCompat
 
 class OdometerService : Service() {
 	private val binder = OdometerBinder()
-	private val random = Random()
+	private var listener: LocationListener? = null
+	private var locManager: LocationManager? = null
+
+	override fun onCreate() {
+		super.onCreate()
+		listener = LocationListener { location ->
+			if (lastLocation == null)
+				lastLocation = location
+			distanceInMeters += location.distanceTo(lastLocation!!)
+			lastLocation = location
+		}
+		locManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+		if (ActivityCompat.checkSelfPermission(
+				this,
+				android.Manifest.permission.ACCESS_FINE_LOCATION
+			) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+				this,
+				android.Manifest.permission.ACCESS_COARSE_LOCATION
+			) == PackageManager.PERMISSION_GRANTED
+		) {
+			val provider = locManager!!.getBestProvider(Criteria(), true)
+			if (provider != null)
+				locManager!!.requestLocationUpdates(provider, 1000, 1f, listener!!)
+		}
+	}
 
 	override fun onBind(intent: Intent): IBinder = binder
 
-	fun getDistance(): Double = random.nextDouble()
+	override fun onDestroy() {
+		super.onDestroy()
+		if (locManager != null && listener != null) {
+			if (ActivityCompat.checkSelfPermission(
+					this,
+					android.Manifest.permission.ACCESS_FINE_LOCATION
+				) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+					this,
+					android.Manifest.permission.ACCESS_COARSE_LOCATION
+				) == PackageManager.PERMISSION_GRANTED
+			)
+				locManager!!.removeUpdates(listener!!)
+			locManager = null
+			listener = null
+		}
+	}
+
+	fun getDistance(): Double = distanceInMeters / 1609.344
 
 	class OdometerBinder : Binder() {
 		fun getOdometer(): OdometerService = OdometerService()
+	}
+
+	companion object {
+		private var distanceInMeters = 0.0
+		private var lastLocation: Location? = null
 	}
 }
